fdf9c6a714eec216d6f568346b351729c1e5e3e4
howisitgoing <howisitgoing@kakao.com>
2023-05-18 11:34:40 +0900

    "refactoring" 예외처리 방법 변경 * @ControllerAdvice를 통해 서비스 레이어에서 발생한 예외를 알맞은 페이지로 예외 출력

--- src/test/java/quokka/todayflowers/domain/service/WithdrawalMemberQueryTest.java
+++ src/test/java/quokka/todayflowers/domain/service/WithdrawalMemberQueryTest.java
@@ -1,7 +1,9 @@
 package quokka.todayflowers.domain.service;
 
 import jakarta.persistence.EntityManager;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,7 +21,7 @@ import java.util.Optional;
 @SpringBootTest
 @ActiveProfiles("test")
 @Transactional
-class withdrawalMemberQueryTest {
+class WithdrawalMemberQueryTest {
     @Autowired
     MemberService memberService;
     @Autowired
